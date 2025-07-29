2f63662e9615e36a380aa82abadb13290823dc7a
BalthazarBujard <balthazar.bujard@gmail.com>
2025-07-29 10:52:26 +0200

    prepared pip package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="concatenate",
+    name="concatenate_BB",
     version="0.1",
     packages=find_packages(),
     description="A custom library for concatenating audio files",
